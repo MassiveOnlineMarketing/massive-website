ccccc0f709ce29e4018908c5a554aa4fb0e6990d
IGuidoo <iguidoo@outlook.com>
2024-03-19 11:57:31 +0100

    blog post links

--- src/payload/blocks/RichText/sterialize.tsx
+++ src/payload/blocks/RichText/sterialize.tsx
@@ -91,13 +91,13 @@ const serialize = (children?: Children): React.ReactNode[] =>
         if (node.fields?.linkType === 'Intern') {
           console.log('node', node)
           return (
-            <InternalAnchor key={i} variant='text' href={`${node.url}/blog` || ''} className=' underline'>
+            <InternalAnchor key={i} variant='text' href={`/blog${node.url}` || ''} className=' underline'>
               {serialize(node?.children)}
             </InternalAnchor>
           )
         } else {
           return (
-            <ExternalAnchor key={i} variant='text' href={`${node.url}/blog` || ''} className=' underline'>
+            <ExternalAnchor key={i} variant='text' href={`${node.url}` || ''} className=' underline'>
               {serialize(node?.children)}
             </ExternalAnchor>
           )}
