ef6e74b8998b1004a73a7787e4d17a3820a3af6a
IGuidoo <iguidoo@outlook.com>
2024-05-08 19:26:56 +0200

    error boundary test

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,22 +1,39 @@
 "use client";
 
 import { Toaster } from "@/website/features/toast/toaster";
+import { ErrorBoundary } from 'react-error-boundary'
+
+function ErrorFallback({ error }: { error: Error }) {
+  return (
+    <div>
+      <h2>Something went wrong:</h2>
+      <details style={{ whiteSpace: 'pre-wrap' }}>
+        {error.toString()}
+        <br />
+        {error.stack}
+      </details>
+    </div>
+  )
+}
+
 
 export async function Providers({ children }: { children: React.ReactNode }) {
   return (
     <>
-      {/* <!-- Google Tag Manager (noscript) --> */}
-      <noscript>
-        <iframe
-          src="https://www.googletagmanager.com/ns.html?id=GTM-NBF7DZQ6"
-          height="0"
-          width="0"
-          className="hidden invisible"
-        ></iframe>
-      </noscript>
-      {/* <!-- End Google Tag Manager (noscript) --> */}
-      {children}
-      <Toaster />
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        {/* <!-- Google Tag Manager (noscript) --> */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NBF7DZQ6"
+            height="0"
+            width="0"
+            className="hidden invisible"
+          ></iframe>
+        </noscript>
+        {/* <!-- End Google Tag Manager (noscript) --> */}
+        {children}
+        <Toaster />
+      </ErrorBoundary>
     </>
   );
 }
