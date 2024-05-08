6bda97646bbd8697ce45ffab7de9e950f8e124e7
IGuidoo <iguidoo@outlook.com>
2024-05-08 19:38:17 +0200

    set re route from app/search --> app/

--- routes.ts
+++ routes.ts
@@ -47,7 +47,7 @@ export const apiAuthPrefix = "/api/auth";
  * The default redirect path after logging in
  * @type {string}
  */
-export const DEFAULT_LOGIN_REDIRECT = "/app/search";
+export const DEFAULT_LOGIN_REDIRECT = "/app";
 
 /**
  * The default route for multi-step form
