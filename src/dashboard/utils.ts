fda4a80030abd24af35944c484bae0369d8283b4
IGuidoo <iguidoo@outlook.com>
2024-05-13 23:58:44 +0200

    update utils url function

--- src/dashboard/utils.ts
+++ src/dashboard/utils.ts
@@ -16,11 +16,17 @@ export function splitAndTrimTextAreaInput(keywords: string): string[] {
 
 /**
  * Replaces the domain URL in a given URL with a forward slash ("/").
+ * If the resulting URL does not start with a "/", it is prepended with one.
  * 
  * @param url - The URL to modify.
  * @param domainUrl - The domain URL to replace.
- * @returns The modified URL with the domain URL replaced by a forward slash.
+ * @returns The modified URL with the domain URL replaced by a forward slash. 
+ *          If the resulting URL does not start with a "/", it is prepended with one.
  */
 export function urlWithoutDomain(url: string, domainUrl: string): string {
-  return url.replace(domainUrl, "/");
+  let result = url.replace(domainUrl, "");
+  if (!result.startsWith("/")) {
+    result = "/" + result;
+  }
+  return result;
 }
